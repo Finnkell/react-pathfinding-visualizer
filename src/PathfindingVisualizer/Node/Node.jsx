13a6cda24e9c074b9ecab61d807bab2703429686
Eric Batista <klose.eric31@gmail.com>
2020-11-15 18:06:13 -0300

    Add Dijkstra algorithm

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -7,17 +7,19 @@ function Node(props) {
   const [row, setRow] = useState(0);
   const [isStart, setIsStart] = useState();
   const [isFinal, setIsFinal] = useState();
+  const [isVisited, setIsVisited] = useState();
 
   const getProps = () => {
     setIsFinal(props.isFinal);
     setIsStart(props.isStart);
+    setIsVisited(props.isVisited);
   }
 
   useEffect(() => {
     getProps();
   }, []);
 
-  const extraClassName = isFinal ? 'node-finish' : isStart ? 'node-start' : '';
+  const extraClassName = isFinal ? 'node-finish' : isStart ? 'node-start' : 'node-visited';
 
   return (
     <div className={`node ${extraClassName}`}>
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,44 +1,101 @@
 import React, { useState, useEffect } from 'react';
+import { dijkstra } from '../Algorithms/Dijkstra';
 import Node from './Node/Node';
 import './PathfindingVisualizer.css';
 
+const START_NODE_COL = 15;
+const START_NODE_ROW = 10;
+const FINAL_NODE_COL = 35;
+const FINAL_NODE_ROW = 10;
+
 function PathfindingVisualizer() {
 
-  const [nodes, setNodes] = useState([]);
+  const [grid, setGrid] = useState([]);
 
-  const loadNodes = () => {
-    const temp = [];
+  const loadInitalGrid = () => {
+    const grid = [];
     for (let row = 0; row < 15; row++) {
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
-        const currentNode = {
-          row,
-          col,
-          isStart: row === 7 && col === 5,
-          isFinal: row === 7 && col === 45,
-        }
-        currentRow.push(currentNode);
+        currentRow.push(createNode(row, col));
       }
-      temp.push(currentRow);
+      grid.push(currentRow);
     }
-    setNodes(temp);
+    setGrid(grid);
+  }
+
+  const createNode = (row, col) => {
+    return {
+      row,
+      col,
+      isStart: row === START_NODE_ROW && col === START_NODE_COL,
+      isFinal: row === FINAL_NODE_ROW && col === FINAL_NODE_COL,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  }
+
+  const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice();
+    const node = newGrid[row][col];
+    const newNode = {
+      ...node,
+      isWall: !node.isWall,
+    };
+    newGrid[row][col] = newNode;
+    return newGrid;
+  }
+
+  const animatedDijkstra = (visitedNodesInOrder) => {
+    for (let i = 0; i < visitedNodesInOrder.length; i++) {
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        const newGrid = grid.slice();
+        const newNode = {
+          ...node,
+          isVisited: true,
+        };
+        newGrid[node.row][node.col] = newNode;
+        setGrid(newGrid);
+      }, 50 * i);
+
+    }
+  }
+
+  const visualizeDijkstra = () => {
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const targetNode = grid[FINAL_NODE_ROW][FINAL_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, targetNode);
+    animatedDijkstra(visitedNodesInOrder);
   }
 
   useEffect(() => {
-    loadNodes();
+    loadInitalGrid();
   }, []);
 
   return (
-    <div className="grid">
-      {nodes.map((row, rowIdx) => {
-        return <div key={rowIdx}>
-          {row.map((node, nodeIdx) => {
-            const { isFinal, isStart } = node;
-            return <Node key={nodeIdx} isStart={isStart} isFinal={isFinal} />
-          })}
-        </div>
-      })}
-    </div>
+    <>
+      {/* <button type="button" onClick={() => visualizeDijkstra()}>
+        Visualize Dijkstra's Algorithm
+      </button> */}
+      <div className="grid">
+        {grid.map((row, rowIdx) => {
+          return <div key={rowIdx}>
+            {row.map((node, nodeIdx) => {
+              const { isFinal, isStart, isVisited } = node;
+              return <Node
+                key={nodeIdx}
+                isStart={isStart}
+                isFinal={isFinal}
+                isVisited={isVisited}
+              />
+            })}
+          </div>
+        })}
+      </div>
+    </>
   );
 }
 
